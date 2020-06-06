69f0c0006200fdd0cd3c0ce7dd31f96f0af5519f
dinhkhang92 <dinh.khang92@hotmail.de>
2020-06-06 17:16:53 +0200

    minor text fixes

--- src/components/Portfolio/Portfolio.js
+++ src/components/Portfolio/Portfolio.js
@@ -14,7 +14,7 @@ function Portfolio(){
                 <p className="portfolio__card-skilltype">Machine Learning</p>
                 <p className="portfolio__card-title">Determination of driver attention using ML-approaches</p>
                 <p className="portfolio__card-description">
-                    This document presents the determination of the driver’s attention by machine learning for the estimation of the collision danger in the
+                    This project presents the determination of the driver’s attention by machine learning for the estimation of the collision danger in the
                     traffic on the basis of real data from endurance measurements.
                 </p>
                 <p className="portfolioe__card-readmore">Read more</p>
@@ -25,8 +25,8 @@ function Portfolio(){
                 <p className="portfolio__card-skilltype">Machine Learning</p>
                 <p className="portfolio__card-title">Image super-resolution using deep neural networks</p>
                 <p className="portfolio__card-description">
-                    This document presents the methods for image super-resolution using artificial neural networks. The approaches are compared and evaluated with
-                    interpolation-based scaling methods such as nearest neighbor, bilinear interpolation and bicubic interpolation.
+                    This project presents methods for image super-resolution using artificial neural networks. The approaches are compared and evaluated with
+                    interpolation-based scaling algorithms such as nearest neighbor, bilinear interpolation and bicubic interpolation.
                 </p>
                 <p className="portfolioe__card-readmore">Read more</p>
             </div>
