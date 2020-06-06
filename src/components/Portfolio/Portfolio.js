256de3e4f064dbdbb0be257084d82a07275b52fa
dinhkhang92 <dinh.khang92@hotmail.de>
2020-06-06 17:13:34 +0200

    added two projects (portfolio)

--- src/components/Portfolio/Portfolio.js
+++ src/components/Portfolio/Portfolio.js
@@ -10,11 +10,25 @@ function Portfolio(){
                 Portfolio
             </h3>
             <div className="portfolio__card">
-                <div className="portfolio__card-img"></div>
-                <h2 className="portfolio__card-title">Determination of driver attention using ML-approaches</h2>
+                <div className="portfolio__card-img sitkrit"></div>
+                <p className="portfolio__card-skilltype">Machine Learning</p>
+                <p className="portfolio__card-title">Determination of driver attention using ML-approaches</p>
                 <p className="portfolio__card-description">
-                    aoöiajdföij
+                    This document presents the determination of the driver’s attention by machine learning for the estimation of the collision danger in the
+                    traffic on the basis of real data from endurance measurements.
                 </p>
+                <p className="portfolioe__card-readmore">Read more</p>
+            </div>
+
+            <div className="portfolio__card">
+                <div className="portfolio__card-img image-sr"></div>
+                <p className="portfolio__card-skilltype">Machine Learning</p>
+                <p className="portfolio__card-title">Image super-resolution using deep neural networks</p>
+                <p className="portfolio__card-description">
+                    This document presents the methods for image super-resolution using artificial neural networks. The approaches are compared and evaluated with
+                    interpolation-based scaling methods such as nearest neighbor, bilinear interpolation and bicubic interpolation.
+                </p>
+                <p className="portfolioe__card-readmore">Read more</p>
             </div>
         </div>
     )
