d79f06eb4310c53acf8ecb438e1bfebe26474375
dinhkhang92 <dinh.khang92@hotmail.de>
2020-06-09 00:38:45 +0200

    up

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -6,6 +6,11 @@ function Footer(){
     return(
         <div className="footer__body">
             <p className="footer__copyright"> &copy; 2020 Khang Dinh </p>
+            <ul className="footer__icons">
+                <li className="footer__xing-icon"></li>
+                <li className="footer__linkedin-icon"></li>
+                <li className="footer__github-icon"></li>
+            </ul>
         </div>
     )
 }
